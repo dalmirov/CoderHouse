199cf8a425ebd4449ad8b3ac78d21c0001f0342f
dalmirovillanueva <dalmirov@gmail.com>
2023-04-11 12:08:20 -0300

    primer microdesafío

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let nombre = prompt ('Como cual es tu nombre?') ;
+let numero = confirm ( nombre + ' querés saber tu número de la suerte? ')
+let letrasNombre = nombre.length
+alert ('tu numero de la suerte es' + ' ' + letrasNombre) 
+
