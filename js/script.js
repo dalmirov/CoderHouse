1e6edabae918d272d8a89c85eec9f7177fee8567
dalmirovillanueva <dalmirov@gmail.com>
2023-04-11 16:19:14 -0300

    resolucion de texto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let nombre = prompt ('Como cual es tu nombre?') ;
+let nombre = prompt ('Cual es tu nombre?') ;
 let numero = confirm ( nombre + ' querés saber tu número de la suerte? ')
 let letrasNombre = nombre.length
 alert ('tu numero de la suerte es' + ' ' + letrasNombre) 
